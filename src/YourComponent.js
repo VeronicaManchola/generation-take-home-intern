014c9ce1e02b353986eb0bc40b9618c10fb5c141
VeronicaManchola <veronica.manchola@hotmail.com>
2019-04-05 16:08:40 -0300

    primer commit

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -4,11 +4,15 @@ import React, { Component } from 'react';
 * Use this component as a launching-pad to build your functionality.
 *
 */
+
+componentDid
+
 class YourComponent extends Component {
   render() {
     return (
       <div style={divStyle}>
         <h1> Put your solution here!</h1>
+        <div id="map"></div>
       </div>
     );
   }
